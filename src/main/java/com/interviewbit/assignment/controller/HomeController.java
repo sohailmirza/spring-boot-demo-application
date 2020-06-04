498b61da44d13da7275a4bb3366e9e408f2de2de
sohailmirza <sohailabbas1000@gmail.com>
2020-06-04 17:23:58 +0530

    Add aop dependency

--- src/main/java/com/interviewbit/assignment/controller/HomeController.java
+++ src/main/java/com/interviewbit/assignment/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.interviewbit.assignment.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HomeController {
 
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("/")
     public String index() {
+
+        logger.info("Index Method");
         return "Spring Boot Basic Application";
+
     }
 }
