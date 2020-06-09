f893f2b625ced42197db851b67ad10c330a3e9d3
sohailmirza <sohailabbas1000@gmail.com>
2020-06-09 19:52:29 +0530

    Add utils

---
+++ src/main/java/com/interviewbit/assignment/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.interviewbit.assignment.utils;
+
+public class Constants {
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+
+    public static final String USERS_END_POINT = "/users";
+}
